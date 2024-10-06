5070c4d75ea542d0ecf23e517f0018d1fa0341f4
Syed Zulqarnain Haider <haidershahpak17@gmail.com>
2024-10-06 20:18:45 +0500

    Footer Responsive

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -5,16 +5,16 @@ import { LocationOn, PhoneAndroid, MailSharp } from '@mui/icons-material';
 const Footer = () => {
     return (
         <>
-        <footer className='bg-black mt-5 py-10'>
+        <footer className='bg-black mt-5 py-10 px-5 sm:px-0'>
             <div className="container mx-auto grid grid-cols-12 gap-5 text-white">
-                <div className="col-span-3">
+                <div className="col-span-12 sm:col-span-6 lg:col-span-3">
                     <h1 className='text-5xl font-bold mb-5'>MOJO</h1>
                     <p className='text-sm text-gray-300 mb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eaque dolor vel natus explicabo laudantium nesciunt exercitationem illum at perspiciatis.</p>
                     <a href='' className='text-sm text-gray-300 block my-3'><LocationOn fontSize='small' /> Lorem ipsum dolor sit amet.</a>
                     <a href='tel:+923278848936' className='text-sm text-gray-300 block my-3'><PhoneAndroid fontSize='small' /> (+92) 327-8848936</a>
                     <a href='mailto:haidershahpak@gmail.com' className='text-sm text-gray-300 block my-3'><MailSharp fontSize='small' /> haidershahpak@gmail.com</a>
                 </div>
-                <div className="col-span-3">
+                <div className="col-span-12 sm:col-span-6 lg:col-span-3">
                     <h4 className='font-bold uppercase mb-5'>Recent Posts</h4>
                     <div className="grid grid-cols-3 border-b-[1px] border-gray-700 py-5 pt-0">
                         <img src="/samsung-camera-post.jpg" alt="" className='w-[75px] h-[75px] object-cover' />
@@ -31,7 +31,7 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-span-2">
+                <div className="col-span-12 sm:col-span-4 lg:col-span-2">
                     <h4 className='font-bold uppercase mb-5'>Our Stores</h4>
                     <ul>
                         <li className='text-sm text-gray-400 hover:text-white duration-300 my-2'><a href="#">Lahore</a></li>
@@ -41,7 +41,7 @@ const Footer = () => {
                         <li className='text-sm text-gray-400 hover:text-white duration-300 my-2'><a href="#">Dubai</a></li>
                     </ul>
                 </div>
-                <div className="col-span-2">
+                <div className="col-span-12 sm:col-span-4 lg:col-span-2">
                     <h4 className='font-bold uppercase mb-5'>Useful links</h4>
                     <ul>
                         <li className='text-sm text-gray-400 hover:text-white duration-300 my-2'><a href="#">Privacy Policy</a></li>
@@ -51,7 +51,7 @@ const Footer = () => {
                         <li className='text-sm text-gray-400 hover:text-white duration-300 my-2'><a href="#">Our Sitemap</a></li>
                     </ul>
                 </div>
-                <div className="col-span-2">
+                <div className="col-span-12 sm:col-span-4 lg:col-span-2">
                     <h4 className='font-bold uppercase mb-5'>Footer menu</h4>
                     <ul>
                         <li className='text-sm text-gray-400 hover:text-white duration-300 my-2'><a href="#">Instagram Profile</a></li>
@@ -70,7 +70,7 @@ const Footer = () => {
 
 const LowerFooter = () => {
     return (
-        <div className="bg-black text-white border-t-[1px] border-gray-700 py-5">
+        <div className="bg-black text-white border-t-[1px] border-gray-700 py-5 px-5 sm:px-0">
             <div className="container mx-auto grid grid-cols-2 lg:grid-cols-3">
                 <p className='text-sm place-content-center'>MOJO © {new Date().getFullYear()} By Atomic Solutions</p>
                 <p className='text-sm text-center place-content-center hidden lg:block'>Syed Zulqarnain Haider</p>
