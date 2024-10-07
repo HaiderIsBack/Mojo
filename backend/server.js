7088c94eaae0af9d21290c54e86e8be709bee31f
Syed Zulqarnain Haider <haidershahpak17@gmail.com>
2024-10-07 13:05:05 +0500

    Testting 3

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use('/compressed', express.static(path.join(__dirname, 'public', 'compressed')));
 
 app.get("/", (req, res) => {
     res.send('Access not allowed!');
